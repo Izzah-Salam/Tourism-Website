8bc28c49513218dd346bfb46e12a4ac6b5f86724
Izzah_Salam <izzah.bscssef20@iba-suk.edu.pk>
2024-02-14 23:46:59 +0500

    adding dynamic date

---
+++ main.js
@@ -0,0 +1,18 @@
+// set date dynamically
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear()
+
+
+
+// close Links
+
+var toggleBtn = document.getElementById("toggle-btn")
+var LinksContainer = document.getElementById("links-container")
+var Links = document.getElementById("links")
+
+
+toggleBtn.addEventListener("click",function(){
+    console.log("you click the toggle btn");
+    LinksContainer.classList.remove("hidden")
+    LinksContainer.classList.toggle("show-links")
+})
