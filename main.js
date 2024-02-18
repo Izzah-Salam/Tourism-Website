c4134ad3b21575718b305f0dacd9efe4ff6cade2
Izzah_Salam <izzah.bscssef20@iba-suk.edu.pk>
2024-02-18 23:45:03 +0500

    adding cross functionality

--- main.js
+++ main.js
@@ -9,9 +9,15 @@ date.innerHTML = new Date().getFullYear()
 var toggleBtn = document.getElementById("toggle-btn")
 var LinksContainer = document.getElementById("links-container")
 var Links = document.getElementById("links")
+var cross = document.getElementById("cross")
 
 
 toggleBtn.addEventListener("click",function(){
     console.log("you click the toggle btn");
     LinksContainer.classList.remove("-translate-y-[100%]")
+})
+
+cross.addEventListener("click",function(){
+    console.log("you click the cross");
+    LinksContainer.classList.add("-translate-y-[100%]")
 })
