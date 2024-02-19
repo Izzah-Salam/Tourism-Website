e2c42f5ec4d1438c6bc69e60e1cefb945dc6e0f3
Izzah_Salam <izzah.bscssef20@iba-suk.edu.pk>
2024-02-19 23:30:03 +0500

    adding fixed navbar functionality

--- main.js
+++ main.js
@@ -4,20 +4,34 @@ date.innerHTML = new Date().getFullYear()
 
 
 
-// close Links
 
 var toggleBtn = document.getElementById("toggle-btn")
 var LinksContainer = document.getElementById("links-container")
 var Links = document.getElementById("links")
 var cross = document.getElementById("cross")
 
-
+// toggle
 toggleBtn.addEventListener("click",function(){
     console.log("you click the toggle btn");
-    LinksContainer.classList.remove("-translate-y-[100%]")
+    LinksContainer.classList.toggle("-translate-y-[100%]")
 })
 
-cross.addEventListener("click",function(){
-    console.log("you click the cross");
-    LinksContainer.classList.add("-translate-y-[100%]")
-})
+
+
+
+// fixed navbad
+// pageoffset is a read only  window property that returns the numbers of pixels 
+// the documents has been scrolled vertically
+
+const body = document.body; // Define the body variable
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+   const scrollHeight = window.pageYOffset;
+   if (scrollHeight > 0) {
+      navbar.style.background = "black";
+
+   } else {
+      navbar.style.background = ""; // Reset to original background color when back to top
+   }
+});
